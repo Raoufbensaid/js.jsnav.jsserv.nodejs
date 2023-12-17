1625a400840abc4e5c263de2a4cfdb0808a39be3
unknown <RaoufBensaid>
2023-12-18 00:40:14 +0100

    Commentaires

--- array.js
+++ array.js
@@ -1,3 +1,21 @@
+/**
+ * Vérifie si l'utilisateur peut conduire (conduite accompagnée incluse)
+ * 
+ * https://en.wikipedia.org/wiki/List_of_minimum_driving_ages
+ * 
+ * @param {number} age
+ * @param {string} country Le pays sur 2 lettres (exemple: "FR", "EN", ...)
+ * @returns {boolean}
+ * @throws {InvalidCountryException} 
+ */
+function canDrive ({age, country}) {
+    // A la réunion un permis de conduite accompagnée existe à partir de 15 ans
+    if (age >= 15 && country === "RE") {
+        return true
+    }
+}
+
+
 export {default as hello} from './hello.js'
 
 export const sum = (items) => items.reduce((acc, item) => acc + item, 0)
