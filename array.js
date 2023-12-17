7f4baeec1d81156cad4a585764cd6db6e5a0bed3
unknown <RaoufBensaid>
2023-12-17 23:46:06 +0100

    Les modules

---
+++ array.js
@@ -0,0 +1,5 @@
+export {default as hello} from './hello.js'
+
+export const sum = (items) => items.reduce((acc, item) => acc + item, 0)
+// export const hello = () => console.log('hello');
+export default () => console.log('HELLO !!');
