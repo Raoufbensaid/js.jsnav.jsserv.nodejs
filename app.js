41fc87672f00bf50da46567e0fa0d412b9da58db
unknown <RaoufBensaid>
2023-12-19 03:56:55 +0100

    JS.nav - Manipuler les DOM

---
+++ app.js
@@ -0,0 +1,26 @@
+// const lis = document.querySelectorAll('li')
+// lis.forEach(v => console.log(v ))
+// console.log(Array.from(lis));
+
+// const ul = document.querySelector('ul li:first-child')
+// console.log(
+//     ul.nodeName,
+//     ul.innerHTML,
+//     ul.innerText,
+//     ul.textContent
+// );
+// ul.setAttribute('hidden', 'hidden')
+// ul.removeAttribute('hidden')
+// console.log(
+//     ul.getAttribute('class')
+// );
+// console.log(
+//     ul.classList.remove('red')
+// );
+
+// setInterval(() => {
+//     ul.classList.toggle('red')
+// }, 1000);
+// setInterval(() => {
+//     ul.classList.add('green')
+// }, 1000);
