9fc0f03def95346357ed5284571580d3285db797
unknown <RaoufBensaid>
2023-12-23 02:28:32 +0100

    lecture ecriture Promises

--- app.js
+++ app.js
@@ -1,3 +1,52 @@
+/*
+F - app.js - 30ko   == Pour un fichier faudra afficher le type, le nom et la taille.
+D - demo            == Pour un dossier faudra afficher seulement le type et la nom.
+F - 
+*/
+
+import { readdir, stat } from "node:fs/promises";
+
+const wait = (duration) => new Promise(resolve => setTimeout(resolve, duration))
+console.time('code')
+
+const files = await readdir('./', {withFileTypes: true})
+await Promise.allSettled(
+    files.map(async (file) => {
+            const parts = [
+                file.isDirectory() ? 'D' : 'F',
+                file.name
+            ]
+            if (!file.isDirectory) {
+                const size = await wait(1000)
+                parts.push(`${size}o`)
+            }
+            console.log(parts.join(' - '));
+        })
+)
+console.timeEnd('code')
+
+
+// import { readdir, stat } from "node:fs/promises";
+
+// const wait = (duration) => new Promise(resolve => setTimeout(resolve, duration))
+// console.time('code')
+
+// const files = await readdir('./', {withFileTypes: true})
+// files.forEach(async (file) => {
+//     const parts = [
+//         file.isDirectory() ? 'D' : 'F',
+//         file.name
+//     ]
+//     if (!file.isDirectory) {
+//         const size = await wait(1000)
+//         parts.push(`${size}o`)
+//     }
+//     console.log(parts.join(' - '));
+// })
+// console.timeEnd('code')
+
+
+
 // import fs from 'node:fs'
 
 // const content = fs.readFile('demo.txt', {encoding: 'utf8'}, function (err, content) {
@@ -30,9 +79,9 @@
 // file.write('HELLO')
 // file.close()
 
-import { watch } from "node:fs/promises";
+// import { watch } from "node:fs/promises";
 
-const watcher = watch('./')
-for await (const event of watcher) {
-    console.log(event);
-}
+// const watcher = watch('./')
+// for await (const event of watcher) {
+//     console.log(event);
+// }
