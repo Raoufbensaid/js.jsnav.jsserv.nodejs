2278aaccacc6508bc77fb836553235e856fa37fc
unknown <RaoufBensaid>
2023-12-24 01:50:16 +0100

    les streams, serveur web http

--- app.js
+++ app.js
@@ -1,29 +1,83 @@
+import { createReadStream, writeFile } from "node:fs";
+import { createServer } from "node:http";
+
+const server = createServer((req, res) => {
+    const url = new URL(req.url, `http://${req.headers.host}`)
+    res.write(`Bonjour ${url.searchParams.get('name')}`)
+    res.end()
+    // console.log(url);
+    // console.log(req.url);
+    // console.log(req.headers.accept)
+    // const file = createReadStream('index.html')
+    // res.writeHead(404, {
+    //     'Content-Type': 'text/html'
+    // })
+    // file.pipe(res)
+    // file.on('end', () => {
+    //     res.end()
+    // })
+    
+    // res.write('hello')
+    // res.end()
+})
+server.listen('8888')
+
+
+
+
+
+// import {  } from "node:fs/promises";
+// import { createReadStream, createWriteStream } from "node:fs";
+
+// const stream = createReadStream('Image.png')
+// const writeStream = createWriteStream('Images.png')
+// stream.pipe(writeStream)
+
+// import { readFile, stat, writeFile } from "node:fs/promises";
+// const content = await readFile('ColisComp-6.png')
+// await writeFile('Image.png', content)
+
+// import { createReadStream } from "node:fs";
+// const stream = createReadStream('Image.png')
+// const {size} = await stat('Image.png')
+// let read = 0
+// stream.on('data', (chunk) => {
+//     read += chunk.length
+//     console.log(Math.round(100 * read / size));
+// })
+// stream.on('close', () => {
+//     console.log('close');
+// })
+
+
+
+
 /*
 F - app.js - 30ko   == Pour un fichier faudra afficher le type, le nom et la taille.
 D - demo            == Pour un dossier faudra afficher seulement le type et la nom.
 F - 
 */
 
-import { readdir, stat } from "node:fs/promises";
+// import { readdir, stat } from "node:fs/promises";
 
-const wait = (duration) => new Promise(resolve => setTimeout(resolve, duration))
-console.time('code')
+// const wait = (duration) => new Promise(resolve => setTimeout(resolve, duration))
+// console.time('code')
 
-const files = await readdir('./', {withFileTypes: true})
-await Promise.allSettled(
-    files.map(async (file) => {
-            const parts = [
-                file.isDirectory() ? 'D' : 'F',
-                file.name
-            ]
-            if (!file.isDirectory) {
-                const size = await wait(1000)
-                parts.push(`${size}o`)
-            }
-            console.log(parts.join(' - '));
-        })
-)
-console.timeEnd('code')
+// const files = await readdir('./', {withFileTypes: true})
+// await Promise.allSettled(
+//     files.map(async (file) => {
+//             const parts = [
+//                 file.isDirectory() ? 'D' : 'F',
+//                 file.name
+//             ]
+//             if (!file.isDirectory) {
+//                 const size = await wait(1000)
+//                 parts.push(`${size}o`)
+//             }
+//             console.log(parts.join(' - '));
+//         })
+// )
+// console.timeEnd('code')
 
 
 // import { readdir, stat } from "node:fs/promises";
